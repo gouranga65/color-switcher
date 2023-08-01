99a0dc4387b7626be08cb2f4f585fdb3a3b9ea97
gouranga <gm49302@gmail.com>
2023-08-01 15:19:05 +0530

    done

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const body = document.getElementsByTagName("body");
+const body = document.querySelector("body");
 const buttons = document.querySelectorAll(".box").forEach((button) => {
-    button.addEventListener('click',(event)=>{
-        if (event.target.class=="white") {
-            console.log("hello");
-        }
-    })
+  button.addEventListener("click", (event) => {
+    if (event.target.id === "grey") {
+      body.style.backgroundColor = event.target.id;
+    } else if (event.target.id === "yellow") {
+      body.style.backgroundColor = event.target.id;
+    } else if (event.target.id === "blue") {
+      body.style.backgroundColor = event.target.id;
+    } else event.target.id === "white";
+    body.style.backgroundColor = event.target.id;
+  });
 });
